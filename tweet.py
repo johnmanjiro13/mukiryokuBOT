99b8eec4904d3b31ef0ac593cff43e4db21252fd
johnmanjiro13 <juntama193@gmail.com>
2018-11-26 21:05:51 +0900

    tweet random speech

--- tweet.py
+++ tweet.py
@@ -1,18 +1,32 @@
 ﻿import os
+import random
 import oauth
 import tweepy
 from tweepy.error import TweepError
 from send_slack import SendSlack
 
 
+SPEECH_PATH = "speeches/speeches.txt"
+
 def post_speech():
     api = oauth.create_twitter_api()
     try:
-        api.update_status("……好きな人の傍いると、辺りに雪を降らせてしまう。そんな女の子がいるそうです（伊勢崎多奈/演劇少女は古都鎌倉を雪で潰す）")
+        speech = get_speech()
+        api.update_status(speech)
     except TweepError as e:
-        service = SendSlack(channel_id=os.getenv("SLACK_MUKIRYOKU_CHANNEL"))
         message = "<!here> mukiryokuBOT raised Error\n" + e.reason
+        service = SendSlack(channel_id=os.getenv("SLACK_MUKIRYOKU_CHANNEL"))
         service.post_slack_message(message, 'mukiryokuBOT')
+
+
+def get_speech():
+    with open(SPEECH_PATH, 'r') as file_object:
+        line = next(file_object)
+        for num, aline in enumerate(file_object, 2):
+            if random.randrange(num): continue
+            line = aline
+        return line
+
 
 if __name__ == '__main__':
     post_speech()
