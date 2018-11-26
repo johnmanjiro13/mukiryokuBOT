ca9f5e8879cd6347348b3b89a9de4571b7b0d228
johnmanjiro13 <juntama193@gmail.com>
2018-11-26 20:03:03 +0900

    send slack

---
+++ send_slack.py
@@ -0,0 +1,20 @@
+﻿import os
+import requests
+
+
+class SendSlack():
+
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+    
+    def post_slack_message(self, message, user_name, emoji=':ghost:'):
+        post_data = {
+            'channel': self.channel_id,
+            'text': message,
+            'username': user_name,
+            'icon_emoji': emoji,
+            'as_user': False
+        }
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        url = os.getenv("SLACK_MUKIRYOKU_ENDPOINT")
+        requests.post(url, headers=headers, json=post_data)
